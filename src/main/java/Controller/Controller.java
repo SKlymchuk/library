8fc4f457d3fb66c96016ef52ce4ddea22cc7296a
Sergiy Klymchuk <ssklymchuk@gmail.com>
2020-02-20 01:57:48 +0200

    Innit

---
+++ src/main/java/Controller/Controller.java
@@ -0,0 +1,15 @@
+package Controller;
+
+import Model.Model;
+import View.View;
+
+public class Controller {
+    private Model model;
+    private View view;
+
+    public Controller(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+}
---
+++ src/main/java/Model/Publication.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Publication {
+    private int year;
+    private String author;
+    private String title;
+    private String subject;
+
+    public Publication(int year, String author, String title, String subject){
+        this.year = year;
+        this.author = author;
+        this.title = title;
+        this.subject = subject;
+    }
+}
